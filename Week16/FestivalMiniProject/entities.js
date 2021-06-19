5b16aa9ee341439543f8723304e02e5a7bfa80bd
MirkoNadj <mirkonadj@yahoo.com>
2021-06-19 18:32:32 +0200

    upgrades_to_Festival_mini_project

--- Week16/FestivalMiniProject/entities.js
+++ Week16/FestivalMiniProject/entities.js
@@ -1,3 +1,22 @@
+// festival
+function Festival () {    
+    this.listOfAllMovies = [];
+    this.listOfAllPrograms = [];
+
+    
+}
+Festival.prototype.addMovie = function (movieObject) {
+    this.listOfAllMovies.push(movieObject);
+    return this.listOfAllMovies.length-1;
+}
+Festival.prototype.addProgram = function (programObject) {
+    this.listOfAllPrograms.push(programObject);
+    return this.listOfAllPrograms.length-1;
+}
+
+
+
+
 // Constructor Movie //
 	
 function Movie (title, genre, length) {    
@@ -45,57 +64,7 @@ function Program (date) {
         return this.date.toDateString()
         };
 
-        //WORK IN PROGRESS
     this.removeMovie = function (deleteIndex) {
-        return this.listOfMovies.splice(0, 1)
+        return this.listOfMovies.splice(deleteIndex, 1)
     }
-    };
-
-
-
-
-
-
-
-
-
-
-/*
-// Constructor Program //
-function Program (date) {
-    this.date = new Date(date);
-    this.listOfMovies = [];
-    this.numberOfMovies = function () {
-        return this.listOfMovies.length;
-    };
-    this.addMovie = function (movieObject) {
-        /*var countGenre = 0;  
-        var sumLength = 0;         
-        this.listOfMovies.forEach(function (movieElement) {
-            sumLength += movieElement.length;
-            if (movieElement.genre === movieObject.genre) {
-                countGenre++;
-            }
-        })
-        if (countGenre < 50 && (sumLength + movieObject.length) <= 480) { 
-            
-        }
-        return this.listOfMovies.push(movieObject);
-    };
-    this.getData = function () {
-        var allMoviesLength = 0;
-        var allMoviesData = '';
-        this.listOfMovies.forEach(function (movieElement) {
-            allMoviesLength += movieElement.length;
-            allMoviesData += movieElement.getData() + '\n\t\t';
-        });
-        return this.date.toDateString() + ', program duration ' + allMoviesLength + ' min \n\t\t' + allMoviesData;
-        };
-    
-    this.getDate = function () {
-        return this.date.toDateString()
-        };	
-    };
-
-
-*/
+};
--- Week16/FestivalMiniProject/form.js
+++ Week16/FestivalMiniProject/form.js
@@ -1,3 +1,5 @@
+var festivalObject = new Festival();
+
 var createMovieBtnElement = document.getElementById('create-movie-btn');
 var createProgramBtnElement = document.getElementById('create-program-btn');
 var addMovieBtnElement = document.getElementById('addmovie-btn');
@@ -6,110 +8,184 @@ var uListOfCreatedPrograms = document.getElementById('programs-list');
 var selectProgramList = document.getElementById('program-dropdown-list');
 var selectMovieList = document.getElementById('movie-dropdown-list');
 var removeMovieFromProgramBtnElement = document.getElementById('remove-movie-btn');
-var listOfCreatedMovies = [];
-var listOfCreatedPrograms = [];
+var deleteMovieBtnElement = document.getElementById('delete-movie-btn');
+var deleteProgramBtnElement = document.getElementById('remove-movie-btn');
+var displayInputError = document.getElementById('input-error');
+var displayMovieCreated = document.getElementById('movie-created');
+var userName = document.getElementById('user-name');
+var userPassword = document.getElementById('password');
+var signInBtn = document.getElementById('sign-in-btn');
+var signOutBtn = document.getElementById('sign-out-btn');
+var divMovie = document.getElementById('divMovie');
+var divProgram = document.getElementById('divProgram');
+var signInOutSwitch = false;
+//console.log(signInOutSwitch)
+
+
+
+// sign In function
+function signIn (){
+	if (userName.value === 'mirko' && userPassword.value === 'bit') {
+	signInOutSwitch = true;
+	alert('Welcome');
+    divMovie.classList.toggle('disabled-div');
+    divProgram.classList.toggle('disabled-div');
+	}
+	else {
+		alert('Wrong User name or Password');
+	} 
+}
+// sign Out function
+function signOut (){
+    if (signInOutSwitch) {
+    divMovie.classList.toggle('disabled-div');
+    divProgram.classList.toggle('disabled-div');
+	signInOutSwitch = false;
+	alert('You loged out')
+    }
+}
 
 // function for creating a new movie
-function createMovie () {
+function createMovie () {	
+	
     var movieTitle = document.getElementById('movie-title').value;
     var movieLength = document.getElementById('movie-length').value;
     var movieGenre = document.getElementById('genre').value;
-
+	
+	// checking input
+	if (!movieTitle || !movieLength || movieGenre === '-') {
+		console.log("error")
+		displayInputError.classList.toggle('showInputError');
+		setTimeout(function(){displayInputError.classList.toggle('showInputError');}, 1000);
+	}
+	else {		
+		displayMovieCreated.classList.toggle('showMovieCreated');
+		setTimeout(function(){displayMovieCreated.classList.toggle('showMovieCreated');}, 1000);
+		
     var movieObject = new Movie(movieTitle, movieGenre, movieLength);
     //console.log(movieObject);
 
-    listOfCreatedMovies.push(movieObject);
+    //listOfCreatedMovies.push(movieObject);
+
+    var indexM = festivalObject.addMovie(movieObject) + 1;
+    console.log(indexM)
+
     // creating new li (movie) and adding it to the ul below the button
     var createdMovieLi = document.createElement('li');
+    createdMovieLi.className = 'createdMovie';
+    var removeMovieBtn = document.createElement('button');
+    removeMovieBtn.className = 'removeMovieBtn';
     createdMovieLi.textContent = movieObject.getData();
+    createdMovieLi.appendChild(removeMovieBtn);
     createdMovieElementUl.appendChild(createdMovieLi);
     // adding new movie to movie dropdown
     var movieOption = document.createElement('option');
     movieOption.textContent = movieObject.getData();
-    movieOption.setAttribute('value', movieObject.getData());
+    movieOption.setAttribute('value', indexM);
     selectMovieList.appendChild(movieOption);
     // reseting inputs
     document.getElementById('movie-title').value = '';
     document.getElementById('movie-length').value = '';
     document.getElementById('genre').value = '-'
+	}
 }
+
+// function for deleting a movie
+
+function deleteMovie () {
+
+    var listOfMovies = document.querySelectorAll('.createdMovie');
+    for (var i = 0; i < listOfMovies.length; i++) {
+        listOfMovies[i].querySelector('button').classList.toggle('on-off');
+        listOfMovies[i].querySelector('button').addEventListener('click', function() {
+            this.closest('.createdMovie').remove();
+        })
+    }
+};
 
 // function for creating a new program
 function createProgram () {
     var programDate = document.getElementById('program-date').value;
     var programObject = new Program(programDate);
-    listOfCreatedPrograms.push(programObject);
+
+
+
+    var indexP = festivalObject.addProgram(programObject) + 1;
+    //listOfCreatedPrograms.push(programObject);
+    console.log(indexP)
+
     //adding to list of programs in html
     var liProgram = document.createElement('li');
-    liProgram.textContent = programObject.getData();
+    liProgram.textContent = programObject.getData();    
     uListOfCreatedPrograms.appendChild(liProgram);
     //adding program select option in html 
     var programOption = document.createElement('option');
     programOption.textContent = programObject.getData();
-    programOption.setAttribute('value', programObject.getDate());
+    programOption.setAttribute('value', indexP);
     selectProgramList.appendChild(programOption);
 };
 
+// function for deleting a program ***WORK IN PROGRESS***
+
+function deleteProgram () {
+    var listOfPrograms = document.querySelectorAll('.createdProgram');
+    for (var i = 0; i < listOfPrograms.length; i++) {
+        listOfPrograms[i].querySelector('button').classList.toggle('on-off');
+        listOfPrograms[i].querySelector('button').addEventListener('click', function() {
+            this.closest('.createdProgram').remove();
+        })
+    }
+
+}
+
+
+
 // function for adding a movie to program
-function addMovieToProgram () {    
-    var movieToAdd = document.getElementById('movie-dropdown-list').value;
-    var toWhichProgram = document.getElementById('program-dropdown-list').value;
-    //console.log('which' + movieToAdd);
-    //console.log(' to' + toWhichProgram);
-    listOfCreatedMovies.forEach(function (movieObjectElement) {
-        //console.log(movieObjectElement.getData())
-        if (movieObjectElement.getData() === movieToAdd) {
-            var movieAdd = movieObjectElement;
-            //console.log(movie);
-            listOfCreatedPrograms.forEach(function (programObjectElement) {
-                //console.log(programObjectElement.getDate())
-                if (programObjectElement.getDate() === toWhichProgram) {
-                programObjectElement.addMovie(movieAdd);
-				
-                // update program list info
-				searchForLi = uListOfCreatedPrograms.getElementsByTagName('li');
-				//console.log(searchForLi)
-				searchItem = toWhichProgram;
-				//console.log(searchItem)
-                    for (i = 0; i < searchForLi.length; i++) {
-                    var currentLi = searchForLi[i].textContent.slice(0,15)
-                    //console.log('trenutni Li ' + currentLi)
-                        if (searchItem === currentLi) {						
-                        searchForLi[i].innerHTML = programObjectElement.getData()
-                        }
-                    }	
-			    }
-            })
-        }
-    })    
-// console.log(listOfCreatedPrograms);    
+function addMovieToProgram () {  
+    var selectedMovieIndex = document.getElementById('movie-dropdown-list').value;
+    console.log(selectedMovieIndex)
+    var movieObjectToAdd = festivalObject.listOfAllMovies[selectedMovieIndex - 1];
+    console.log(movieObjectToAdd)
+    
+    //var movieToAdd = document.getElementById('movie-dropdown-list').value;
+    var selectedProgramIndex = document.getElementById('program-dropdown-list').value;
+    var programObjectToReceive = festivalObject.listOfAllPrograms[selectedProgramIndex - 1];
+
+    programObjectToReceive.addMovie(movieObjectToAdd);
+
+    searchForLi = uListOfCreatedPrograms.getElementsByTagName('li');
+    searchForLi[selectedProgramIndex - 1].innerHTML = programObjectToReceive.getData();    
+
+    searchForOption = selectProgramList.getElementsByTagName('option');
+    searchForOption[selectedProgramIndex].textContent = programObjectToReceive.getData();
+
+   
 };
 
-// function for removing a movie from program    WORK IN PROGRESS
+// function for removing a movie from program
 
 function removeMovieFromProgram () {    
     var movieToRemove = document.getElementById('movie-dropdown-list').value;
     var fromWhichProgram = document.getElementById('program-dropdown-list').value;
-    //console.log('which' + movieToAdd);
-    //console.log(' to' + toWhichProgram);
+    
     listOfCreatedMovies.forEach(function (movieObjectElement) {
-        //console.log(movieObjectElement.getData())
+        
         if (movieObjectElement.getData() === movieToRemove) {
-            var deleteIndex = indexOf(movieObjectElement);
-            //console.log(movie);
+            var deleteIndex = listOfCreatedMovies.indexOf(movieObjectElement);
+            
             listOfCreatedPrograms.forEach(function (programObjectElement) {
-                //console.log(programObjectElement.getDate())
+                
                 if (programObjectElement.getDate() === fromWhichProgram) {
-                programObjectElement.removeMovie(movieRemove);
+                programObjectElement.removeMovie(deleteIndex);
 				
                     // update program list info
                     searchForLi = uListOfCreatedPrograms.getElementsByTagName('li');
-                    console.log(searchForLi)
+                    
                     searchItem = fromWhichProgram;
-                    console.log(searchItem)
+                    
                     for (i = 0; i < searchForLi.length; i++) {
                     var currentLi = searchForLi[i].textContent.slice(0,15)
-                    console.log('trenutni Li ' + currentLi)
+                    
                         if (searchItem === currentLi) {						
                         searchForLi[i].innerHTML = programObjectElement.getData()
                         }
@@ -121,14 +197,21 @@ function removeMovieFromProgram () {
     console.log(listOfCreatedPrograms);    
 }
 
-
 // event listeners
+signInBtn.addEventListener('click', signIn)
+signOutBtn.addEventListener('click', signOut)
 
+if (signInOutSwitch = true) {
 createMovieBtnElement.addEventListener('click', createMovie);
 createProgramBtnElement.addEventListener('click', createProgram);
 addMovieBtnElement.addEventListener('click',addMovieToProgram);
 
 removeMovieFromProgramBtnElement.addEventListener('click', removeMovieFromProgram);
+deleteMovieBtnElement.addEventListener('click', deleteMovie);
+deleteProgramBtnElement.addEventListener('click', deleteProgram);
+}
+
+
 
 
 
